05e98aac2ed01ba71e019aaae0c5dd1b79573671
Maria Lucena <mpiantella@hotmail.com>
2022-01-18 14:14:53 -0500

    clean up

--- packages/cloud/cft/deploy-cft.sh
+++ packages/cloud/cft/deploy-cft.sh
@@ -3,8 +3,5 @@
 stackName='Remote-Brilliance-Tiffin'
 # will start from ~/cloud
 cd cft
-# aws cloudformation create-stack --stack-name $stackName --template-body remote-brilliance.yaml \
-# --capabilities CAPABILITY_NAMED_IAM 
-#--parameters ParameterKey=APIName,ParameterValue=ProdNotesAPI
 echo $stackName
 aws cloudformation deploy --stack-name $stackName --template-file remote-brilliance.yaml --capabilities CAPABILITY_NAMED_IAM 
