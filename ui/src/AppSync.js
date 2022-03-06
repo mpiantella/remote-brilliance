e4870dd885bf7da376ec948b3d63eadf1fedd70e
Maria Lucena <mpiantella@hotmail.com>
2022-03-06 18:27:24 -0500

    clean up

--- ui/src/AppSync.js
+++ ui/src/AppSync.js
@@ -1,6 +1,6 @@
 export default {
-  "graphqlEndpoint": "https://o3rgmp4a3rb5piqgi7jn54csvu.appsync-api.us-east-1.amazonaws.com/graphql",
+  "graphqlEndpoint": "https://YOUR-UNIQUE-ENDPOINT.appsync-api.us-east-1.amazonaws.com/graphql",
   "region": "us-east-1",
   "authenticationType": "API_KEY",
-  "apiKey": "da2-seezfno4azgxzdcm6a4d4akbjm"
+  "apiKey": "da2-YOUR_OWN_KEY"
 }
